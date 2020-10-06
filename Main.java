10275eac1183644a2fc52783e2a6d0e515c7b33c
nurtt0949 <71143068+nurtt0949@users.noreply.github.com>
2020-10-06 09:50:12 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *multiplication table until 12
+ * @author Tolga
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a Scanner
+     Scanner input = new Scanner(System.in);
+    //ask user for integer to create multiplication table
+     System.out.println("Please enter an integer to create a multiplication table for:");
+    //declare a variable for users integer
+     int integer = input.nextInt();
+    //for loop for multiplication table
+     for(int count = 1; count <= 12; count++){
+     //declare a variable for total number
+     int total = count * integer;
+     //loop action: print the number 
+     System.out.println(integer + " x " + count + " = " + total);
+     }
+    
     
   }
 }
+
